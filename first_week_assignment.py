2646708e571f518ebbdf99da58e90cdd6fe20022
thimzy <timilehinayegbusi@gmail.com>
2022-01-24 00:19:35 +0100

    Assignment

---
+++ first_week_assignment.py
@@ -0,0 +1,19 @@
+my_list = ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog."]
+
+list = " ".join(my_list)
+print(list)
+
+sample_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print(sample_list [1:4])
+
+color_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+color = input("Enter your color: ")
+
+color_list.append(color)
+print(color_list)
+
+new_list = ['this', "brown", 55, "oxen", True, 0.85]
+
+new_list [4] = "false"
+print(new_list)
